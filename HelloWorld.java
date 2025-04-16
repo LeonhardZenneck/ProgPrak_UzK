e0c057c81079cb728a2b352a5eb6ca886a038be4
Leonhard Zenneck <leo.zenneck@tum.de>
2025-04-16 11:21:12 +0200

    update

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("This is a simple Java program.");
+        System.out.println("It prints a greeting message to the console.");
+        System.out.println("Java is a versatile programming language.");
+        System.out.println("Happy coding!");
+    }
+}
